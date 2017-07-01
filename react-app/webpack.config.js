ae21436c09304fc8bfcca2a188e83c5fb047a585
Kristan 'Krispy' Uccello <kuccello@gmail.com>
2017-07-01 14:46:21 -0700

    Fix react-app build with webpack

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -42,7 +42,9 @@ module.exports = {
   },
   target: 'electron-renderer',
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
